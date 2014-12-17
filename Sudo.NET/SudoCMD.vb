9f80d60fa59dcd1247cf4b2e6c38511717b063a9
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2014-12-18 01:51:47 +0200

    Added SudoCMD code

--- Sudo.NET/SudoCMD.vb
+++ Sudo.NET/SudoCMD.vb
@@ -1,7 +1,25 @@
 ﻿Module SudoCMD
+    Dim isFirst As Boolean = True
+    Dim ProgramToLaunch As String = ""
+    Dim args As String = ""
+
+    Dim objShell As Object = CreateObject("Shell.Application")
 
     Sub Main()
+        For Each s As String In My.Application.CommandLineArgs
+            If isFirst Then
+                ProgramToLaunch = s
+                isFirst = False
+            Else
+                args = args & s
+            End If
+        Next
 
+        objShell.ShellExecute(ProgramToLaunch, args, "", "runas")
+
+        Do Until 0 <> 0
+            End
+        Loop
     End Sub
 
 End Module
