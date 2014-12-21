a188e37f208e26f0e19a16dcbb0cdaf89578d282
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2014-12-21 20:28:05 +0200

    Added SteamIDLinker code

--- Learn VB Android app/SteamIDLinker.Designer.vb
+++ Learn VB Android app/SteamIDLinker.Designer.vb
@@ -22,17 +22,143 @@ Partial Class SteamIDLinker
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SteamIDLinker))
+        Me.cbSteamGameID = New System.Windows.Forms.ComboBox()
+        Me.imgLoading = New System.Windows.Forms.PictureBox()
+        Me.lblGameName = New System.Windows.Forms.Label()
+        Me.imgGameImage = New System.Windows.Forms.PictureBox()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.btnRunGameID = New System.Windows.Forms.Button()
+        Me.btnInstall = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.WebBrowserGamePage = New System.Windows.Forms.WebBrowser()
+        CType(Me.imgLoading, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.imgGameImage, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'cbSteamGameID
+        '
+        Me.cbSteamGameID.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cbSteamGameID.Items.AddRange(New Object() {"60", "220", "440", "620", "730"})
+        Me.cbSteamGameID.Location = New System.Drawing.Point(12, 12)
+        Me.cbSteamGameID.MaxDropDownItems = 50
+        Me.cbSteamGameID.Name = "cbSteamGameID"
+        Me.cbSteamGameID.Size = New System.Drawing.Size(461, 21)
+        Me.cbSteamGameID.TabIndex = 0
+        Me.cbSteamGameID.Text = "Game ID"
+        '
+        'imgLoading
+        '
+        Me.imgLoading.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.imgLoading.Location = New System.Drawing.Point(12, 37)
+        Me.imgLoading.Name = "imgLoading"
+        Me.imgLoading.Size = New System.Drawing.Size(17, 17)
+        Me.imgLoading.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.imgLoading.TabIndex = 47
+        Me.imgLoading.TabStop = False
+        '
+        'lblGameName
+        '
+        Me.lblGameName.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblGameName.AutoEllipsis = True
+        Me.lblGameName.Location = New System.Drawing.Point(12, 40)
+        Me.lblGameName.Name = "lblGameName"
+        Me.lblGameName.Size = New System.Drawing.Size(461, 14)
+        Me.lblGameName.TabIndex = 48
+        Me.lblGameName.Text = "Enter a Game ID above"
+        '
+        'imgGameImage
+        '
+        Me.imgGameImage.Cursor = System.Windows.Forms.Cursors.Default
+        Me.imgGameImage.ImageLocation = ""
+        Me.imgGameImage.InitialImage = CType(resources.GetObject("imgGameImage.InitialImage"), System.Drawing.Image)
+        Me.imgGameImage.Location = New System.Drawing.Point(12, 89)
+        Me.imgGameImage.Name = "imgGameImage"
+        Me.imgGameImage.Size = New System.Drawing.Size(460, 215)
+        Me.imgGameImage.TabIndex = 0
+        Me.imgGameImage.TabStop = False
+        '
+        'btnRun
+        '
+        Me.btnRun.Enabled = False
+        Me.btnRun.Location = New System.Drawing.Point(12, 60)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(75, 23)
+        Me.btnRun.TabIndex = 49
+        Me.btnRun.Text = "Run"
+        Me.btnRun.UseVisualStyleBackColor = True
+        '
+        'btnRunGameID
+        '
+        Me.btnRunGameID.Enabled = False
+        Me.btnRunGameID.Location = New System.Drawing.Point(93, 60)
+        Me.btnRunGameID.Name = "btnRunGameID"
+        Me.btnRunGameID.Size = New System.Drawing.Size(75, 23)
+        Me.btnRunGameID.TabIndex = 50
+        Me.btnRunGameID.Text = "RunGameID"
+        Me.btnRunGameID.UseVisualStyleBackColor = True
+        '
+        'btnInstall
+        '
+        Me.btnInstall.Enabled = False
+        Me.btnInstall.Location = New System.Drawing.Point(174, 60)
+        Me.btnInstall.Name = "btnInstall"
+        Me.btnInstall.Size = New System.Drawing.Size(75, 23)
+        Me.btnInstall.TabIndex = 51
+        Me.btnInstall.Text = "Install"
+        Me.btnInstall.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(255, 60)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 52
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'WebBrowserGamePage
+        '
+        Me.WebBrowserGamePage.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebBrowserGamePage.Location = New System.Drawing.Point(12, 310)
+        Me.WebBrowserGamePage.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowserGamePage.Name = "WebBrowserGamePage"
+        Me.WebBrowserGamePage.Size = New System.Drawing.Size(461, 377)
+        Me.WebBrowserGamePage.TabIndex = 53
         '
         'SteamIDLinker
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ClientSize = New System.Drawing.Size(485, 699)
+        Me.Controls.Add(Me.WebBrowserGamePage)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnInstall)
+        Me.Controls.Add(Me.btnRunGameID)
+        Me.Controls.Add(Me.btnRun)
+        Me.Controls.Add(Me.cbSteamGameID)
+        Me.Controls.Add(Me.imgLoading)
+        Me.Controls.Add(Me.lblGameName)
+        Me.Controls.Add(Me.imgGameImage)
         Me.Name = "SteamIDLinker"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "SteamIDLinker"
+        CType(Me.imgLoading, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.imgGameImage, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents cbSteamGameID As System.Windows.Forms.ComboBox
+    Friend WithEvents imgLoading As System.Windows.Forms.PictureBox
+    Friend WithEvents lblGameName As System.Windows.Forms.Label
+    Friend WithEvents imgGameImage As System.Windows.Forms.PictureBox
+    Friend WithEvents btnRun As System.Windows.Forms.Button
+    Friend WithEvents btnRunGameID As System.Windows.Forms.Button
+    Friend WithEvents btnInstall As System.Windows.Forms.Button
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+    Friend WithEvents WebBrowserGamePage As System.Windows.Forms.WebBrowser
 End Class
--- Learn VB Android app/SteamIDLinker.vb
+++ Learn VB Android app/SteamIDLinker.vb
@@ -1,3 +1,70 @@
 ﻿Public Class SteamIDLinker
+    Private Sub MsgNoGameID()
+        MsgBox("Please fill in a Game ID!", MsgBoxStyle.Critical, "No Game ID entered")
+        Me.BringToFront()
+        cbSteamGameID.SelectAll()
+    End Sub
+    ' var chosenGameURL = 'http://store.steampowered.com/app/' + chosenGame + '/';
+    'var chosenGameImage = 'http://cdn.akamai.steamstatic.com/steam/apps/' + chosenGame + '/header.jpg';
 
+    Private Sub cbSteamGameID_TextChanged(sender As Object, e As EventArgs) Handles cbSteamGameID.TextChanged
+        If cbSteamGameID.Text = "Game ID" Or cbSteamGameID.Text = "" Or Len(cbSteamGameID.Text) < 2 Then
+            imgLoading.Visible = False
+            lblGameName.Text = "Enter a Game ID above"
+        Else
+            WebBrowserGamePage.Navigate("https://www.youtube.com/embed/" & cbSteamGameID.Text & "?autoplay=0")
+            imgLoading.Visible = True
+            lblGameName.Text = "      Loading..."
+            btnRun.Enabled = True
+            btnRunGameID.Enabled = True
+            btnInstall.Enabled = True
+            imgGameImage.ImageLocation = "http://cdn.akamai.steamstatic.com/steam/apps/" & cbSteamGameID.Text & "/header.jpg"
+        End If
+    End Sub
+
+    Private Sub cbSteamGameID_Enter(sender As Object, e As EventArgs) Handles cbSteamGameID.MouseDown
+        If cbSteamGameID.Text = "Game ID" Then
+            cbSteamGameID.Text = ""
+        Else
+            cbSteamGameID.SelectAll()
+        End If
+    End Sub
+
+    Private Sub txtComboVID_Leave(sender As Object, e As EventArgs) Handles cbSteamGameID.Leave
+        If cbSteamGameID.Text = "" Then
+            cbSteamGameID.Text = "Game ID"
+        ElseIf cbSteamGameID.Text <> "Game ID" Then
+            If Not cbSteamGameID.Items.Contains(cbSteamGameID.Text) Then
+                cbSteamGameID.Items.Add(cbSteamGameID.Text)
+            End If
+        End If
+    End Sub
+
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+        If cbSteamGameID.Text = "Game ID" Or cbSteamGameID.Text = "" Then
+            MsgNoGameID()
+        Else
+            MsgBox("steam://run/" & cbSteamGameID.Text)
+        End If
+    End Sub
+
+    Private Sub btnRunGameID_Click(sender As Object, e As EventArgs) Handles btnRunGameID.Click
+        If cbSteamGameID.Text = "Game ID" Or cbSteamGameID.Text = "" Then
+            MsgNoGameID()
+        Else
+            MsgBox("steam://rungameid/" & cbSteamGameID.Text)
+        End If
+    End Sub
+
+    Private Sub btnInstall_Click(sender As Object, e As EventArgs) Handles btnInstall.Click
+        If cbSteamGameID.Text = "Game ID" Or cbSteamGameID.Text = "" Then
+            MsgNoGameID()
+        Else
+            MsgBox("steam://install/" & cbSteamGameID.Text)
+        End If
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
 End Class
