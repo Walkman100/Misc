e6b2005d76ba4965ad736902a9f3084a91f3359c
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2015-01-15 22:25:16 +0200

    Moved files around

--- Scripts folder BETA/Install.bat
+++
@@ -1,4 +0,0 @@
-mode con: cols=14 lines=1
-@echo Set objShell = CreateObject("Shell.Application") > %temp%\sudo.tmp.vbs
-@echo objShell.ShellExecute "%~dp0\Install2.bat", "", "", "runas" >> %temp%\sudo.tmp.vbs
-@cscript %temp%\sudo.tmp.vbs
--- Scripts folder BETA/Install.vbs
+++ Scripts folder BETA/Install.vbs
@@ -1,2 +1,2 @@
 Set objShell = CreateObject("Shell.Application") 
-objShell.ShellExecute "Install2.bat", "", "", "runas" 
+objShell.ShellExecute "data\gen.bat", "", "", "runas" 
