2c6012d5d2c2890327b4302815ea3d2c2bf8c2e8
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2015-05-04 18:59:47 +0200

    Quick bugfix (Doh)

--- Binaries/sudo.cmd
+++ Binaries/sudo.cmd
@@ -1,3 +1,3 @@
-@echo args = Right("%*", (Len("%*") - Len("%1"))) >> %temp%\sudo.tmp.vbs
+@echo args = Right("%*", (Len("%*") - Len("%1"))) > %temp%\sudo.tmp.vbs
 @echo CreateObject("Shell.Application").ShellExecute "%1", args, "", "runas" >> %temp%\sudo.tmp.vbs
 @cscript %temp%\sudo.tmp.vbs
--- Binaries/sudoRED.cmd
+++ Binaries/sudoRED.cmd
@@ -1,3 +1,3 @@
 mode con: cols=14 lines=1
-@echo CreateObject("Shell.Application").ShellExecute "%ProgramFiles%\Remove Empty Directories\RED2.exe", ""%1"", "", "runas" >> %temp%\sudo.tmp.vbs
+@echo CreateObject("Shell.Application").ShellExecute "%ProgramFiles%\Remove Empty Directories\RED2.exe", ""%1"", "", "runas" > %temp%\sudo.tmp.vbs
 @cscript %temp%\sudo.tmp.vbs
