e59080f938ba9743bcb311c7b23c622bbdac7270
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2015-05-04 19:15:54 +0200

    Updated gitignore, removed some @'s and added an echo off to registry key generator batfile

--- Binaries/RedContextMenu-data/gen.bat
+++ Binaries/RedContextMenu-data/gen.bat
@@ -1,4 +1,5 @@
-@echo Generating registry keys...
+@echo off
+echo Generating registry keys...
 
 @echo Windows Registry Editor Version 5.00 > %temp%\REDContext.reg
 @echo. >> %temp%\REDContext.reg
@@ -17,5 +18,5 @@
 @echo [HKEY_CLASSES_ROOT\Folder\shell\Remove empty dirs (ADMIN)\command] >> %temp%\REDContext.reg
 @echo @="sudoRED \"%%1\"" >> %temp%\REDContext.reg
 
-@echo Generating registry keys done, attempting to merge...
-@%temp%\REDContext.reg
+echo Generating registry keys done, attempting to merge...
+%temp%\REDContext.reg
