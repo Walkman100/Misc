8e24cc023f2b51d40a35c53fe27c3d2a9b44974a
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2015-08-16 09:18:26 +0200

    Removed unnecessary files from Sudo.NET, combined SudoGUI.Designer with SudoGUI.vb

--- Sudo.NET/SudoGUI.vb
+++ Sudo.NET/SudoGUI.vb
@@ -1,4 +1,6 @@
-﻿Public Class SudoGUI
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Public Class SudoGUI
+    Inherits System.Windows.Forms.Form
     Dim isFirst As Boolean = True
     Dim ProgramToLaunch As String = ""
     Dim args As String = ""
@@ -19,5 +21,32 @@
             Application.Exit()
             End
         Loop
+    End Sub
+    
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+    Private components As System.ComponentModel.IContainer
+    Private Sub InitializeComponent()
+        Me.SuspendLayout
+        'SudoGUI
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1, 1)
+        Me.ControlBox = false
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = false
+        Me.MinimizeBox = false
+        Me.ShowIcon = false
+        Me.ShowInTaskbar = false
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ResumeLayout(false)
     End Sub
 End Class
